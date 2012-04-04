9b8aec8c84d0f4d0d83b84460cc2bb9b038f8429
Michael Mosmann <michael@mosmann.de>
2012-04-04 08:15:46 +0200

    lic header update - welcome martin

--- src/test/java/de/flapdoodle/embedmongo/distribution/DistributionTest.java
+++ src/test/java/de/flapdoodle/embedmongo/distribution/DistributionTest.java
@@ -1,5 +1,7 @@
 /**
- * Copyright (C) 2011 Michael Mosmann <michael@mosmann.de>
+ * Copyright (C) 2011
+ *   Michael Mosmann <michael@mosmann.de>
+ *   Martin Jöhren <m.joehren@googlemail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package de.flapdoodle.embedmongo.distribution;
 
 import junit.framework.TestCase;
