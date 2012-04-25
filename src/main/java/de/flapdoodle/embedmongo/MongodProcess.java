afde2d3c62f7f9b327c2f2e4bbecda4d427e83e3
Michael Mosmann <michael@mosmann.de>
2012-04-25 08:10:22 +0200

    windows process db shutdown

--- src/main/java/de/flapdoodle/embedmongo/MongodProcess.java
+++ src/main/java/de/flapdoodle/embedmongo/MongodProcess.java
@@ -1,14 +1,14 @@
 /**
  * Copyright (C) 2011
- *   Michael Mosmann <michael@mosmann.de>
- *   Martin Jöhren <m.joehren@googlemail.com>
- *
+ * Michael Mosmann <michael@mosmann.de>
+ * Martin Jöhren <m.joehren@googlemail.com>
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,15 +21,26 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.Bytes;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+import com.mongodb.QueryBuilder;
+import com.mongodb.WriteConcern;
 
 import de.flapdoodle.embedmongo.config.MongodConfig;
 import de.flapdoodle.embedmongo.distribution.Distribution;
+import de.flapdoodle.embedmongo.distribution.Platform;
 import de.flapdoodle.embedmongo.io.ConsoleOutput;
 import de.flapdoodle.embedmongo.io.LogWatch;
 import de.flapdoodle.embedmongo.runtime.NUMA;
@@ -49,10 +60,13 @@ public class MongodProcess {
 	boolean _processKilled = false;
 	boolean _stopped = false;
 
+	private Distribution _distribution;
+
 	public MongodProcess(Distribution distribution, MongodConfig config, MongodExecutable mongodExecutable)
 			throws IOException {
 		_config = config;
 		_mongodExecutable = mongodExecutable;
+		_distribution=distribution;
 
 		try {
 			File dbDir;
@@ -62,13 +76,13 @@ public class MongodProcess {
 				dbDir = Files.createTempDir("embedmongo-db");
 				_dbDir = dbDir;
 			}
-//			ProcessBuilder processBuilder = new ProcessBuilder(enhanceCommandLinePlattformSpecific(distribution,
-//					getCommandLine(_config, _mongodExecutable.getFile(), dbDir)));
-//			processBuilder.redirectErrorStream();
-//			_process = new ProcessControl(processBuilder.start());
+			//			ProcessBuilder processBuilder = new ProcessBuilder(enhanceCommandLinePlattformSpecific(distribution,
+			//					getCommandLine(_config, _mongodExecutable.getFile(), dbDir)));
+			//			processBuilder.redirectErrorStream();
+			//			_process = new ProcessControl(processBuilder.start());
 			_process = ProcessControl.fromCommandLine(enhanceCommandLinePlattformSpecific(distribution,
 					getCommandLine(_config, _mongodExecutable.getFile(), dbDir)));
-			
+
 			Runtime.getRuntime().addShutdownHook(new JobKiller());
 
 			if (LogWatch.waitForStart(_process.getReader(), "waiting for connections on port", "failed", 20000)) {
@@ -99,7 +113,7 @@ public class MongodProcess {
 		if (NUMA.isNUMA(distribution.getPlatform())) {
 			switch (distribution.getPlatform()) {
 				case Linux:
-					List<String> ret=new ArrayList<String>();
+					List<String> ret = new ArrayList<String>();
 					ret.add("numactl");
 					ret.add("--interleave=all");
 					ret.addAll(commands);
@@ -113,6 +127,9 @@ public class MongodProcess {
 
 	public synchronized void stop() {
 		if (!_stopped) {
+
+			sendStopToMongoInstance();
+
 			if (_process != null) {
 				_process.stop();
 			}
@@ -126,6 +143,27 @@ public class MongodProcess {
 					_stopped = true;
 					_logger.warning("Could not delete mongod executable NOW: " + _mongodExecutable.getFile());
 				}
+			}
+		}
+	}
+
+	private void sendStopToMongoInstance() {
+		if (_distribution.getPlatform()==Platform.Windows) {
+			_logger.warning("\n" +
+					"------------------------------------------------\n" +
+					"On windows stopping mongod process could take too much time.\n" +
+					"We will send shutdown to db for speedup.\n" +
+					"This will cause some logging of exceptions which we can not suppress.\n" +
+					"------------------------------------------------");
+			try {
+				Mongo mongo = new Mongo("localhost", _config.getPort());
+				DB db = mongo.getDB("admin");
+	//			db.doEval("db.shutdownServer();");
+				db.command(new BasicDBObject("shutdown", 1).append("force", true));
+			} catch (UnknownHostException e) {
+				_logger.log(Level.SEVERE, "sendStop", e);
+			} catch (MongoException e) {
+	//			_logger.log(Level.SEVERE, "sendStop", e);
 			}
 		}
 	}
