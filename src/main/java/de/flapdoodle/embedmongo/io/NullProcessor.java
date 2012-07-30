bcd979ca6903c090ba7fb548818bff4b6091df6a
Michael Mosmann <michael@mosmann.de>
2012-07-30 22:27:27 +0200

    fix typo

--- src/main/java/de/flapdoodle/embedmongo/io/NullProcessor.java
+++ src/main/java/de/flapdoodle/embedmongo/io/NullProcessor.java
@@ -18,6 +18,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package de.flapdoodle.embedmongo.io;
+
 import de.flapdoodle.embedmongo.io.IStreamProcessor;
 
 /**
