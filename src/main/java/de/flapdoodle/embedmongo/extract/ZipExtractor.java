424ba3a201d4fb916f4d4df9129c09632ba44f03
Michael Mosmann <michael@mosmann.de>
2012-07-30 22:14:48 +0200

    lic header changes

--- src/main/java/de/flapdoodle/embedmongo/extract/ZipExtractor.java
+++ src/main/java/de/flapdoodle/embedmongo/extract/ZipExtractor.java
@@ -3,6 +3,9 @@
  *   Michael Mosmann <michael@mosmann.de>
  *   Martin Jöhren <m.joehren@googlemail.com>
  *
+ * with contributions from
+ * 	konstantin-ba@github,
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
